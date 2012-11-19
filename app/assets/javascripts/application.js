b2f14fcd9bd3cc453cfad8e91555b5a6d1f7289f
Jevon Yeoh <jevonyeoh@gmail.com>
2012-11-18 21:14:20 -0500

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$('#hideshow').toggle(function() {
+	$('#hideshow').text('Show');
+		$('#message').hide();
+	}, function() {
+		$('#hideshow').text('Hide');
+		$('#message').show();
+	}
+);
